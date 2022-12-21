929a2c5148975a4191e640a0987eb43ac8433fc3
hidehalo <tianchen_cc@yeah.net>
2022-12-21 21:59:10 +0800

    update

--- lib/song/id3.ts
+++ lib/song/id3.ts
@@ -7,47 +7,47 @@ import fs from 'fs';
 
 // TODO: 支持ID3 tags的写入
 function writeID3Tags() {
-  try {
-    console.log("do my things");
-    const filepath =
-    '/Users/tianchen/Music/网易云音乐测试/Manami/ベストフレンド/ベストフレンド ~琉球ver.~.mp3';
-    let imageBuffer = Buffer.from([]);
-    https.get('https://p1.music.126.net/05CrrjiAJX8TrPKIuN3Vyg==/109951163556308776.jpg', (dlResp) => {
-      let chunks = [] as Uint8Array[];
-      dlResp.on('data', (chunk) => {
-        chunks.push(chunk);
-      });
-      dlResp.on('end', async () => {
-        imageBuffer = Buffer.concat(chunks);
-      })
-    });
-    const tags = {
-      title: "爱一个人好难111",
-      artist: "张亚飞111",
-      album: "她的时光111",
-      TRCK: "1",
-      image: {
-        mime: 'image/jpeg',
-        type: {
-          id: 1,
-          name: '109951163556308776.jpg'
-        },
-        description: '',
-        imageBuffer
-      }
-    } as NodeID3.Tags;
-    return NodeID3.update(tags, filepath, {}, (err) => {
-      console.error(err);
-    });
-    // const buffer = fs.readFileSync(filepath);
-    // const mp3tag = new MP3Tag(buffer, true);
-    // mp3tag.tags.v2.TPE1 = "张亚飞111";
-    // mp3tag.tags.v2.TIT2 = "爱一个人好难111";
-    // mp3tag.tags.v2.TALB = "她的时光111";
-    // mp3tag.save();
-  } catch (e) {
-    console.error(e);
-  }
+  // try {
+  //   console.log("do my things");
+  //   const filepath =
+  //   '/Users/tianchen/Music/网易云音乐测试/Manami/ベストフレンド/ベストフレンド ~琉球ver.~.mp3';
+  //   let imageBuffer = Buffer.from([]);
+  //   https.get('https://p1.music.126.net/05CrrjiAJX8TrPKIuN3Vyg==/109951163556308776.jpg', (dlResp) => {
+  //     let chunks = [] as Uint8Array[];
+  //     dlResp.on('data', (chunk) => {
+  //       chunks.push(chunk);
+  //     });
+  //     dlResp.on('end', async () => {
+  //       imageBuffer = Buffer.concat(chunks);
+  //     })
+  //   });
+  //   const tags = {
+  //     title: "爱一个人好难111",
+  //     artist: "张亚飞111",
+  //     album: "她的时光111",
+  //     TRCK: "1",
+  //     image: {
+  //       mime: 'image/jpeg',
+  //       type: {
+  //         id: 1,
+  //         name: '109951163556308776.jpg'
+  //       },
+  //       description: '',
+  //       imageBuffer
+  //     }
+  //   } as NodeID3.Tags;
+  //   return NodeID3.update(tags, filepath, {}, (err) => {
+  //     console.error(err);
+  //   });
+  //   // const buffer = fs.readFileSync(filepath);
+  //   // const mp3tag = new MP3Tag(buffer, true);
+  //   // mp3tag.tags.v2.TPE1 = "张亚飞111";
+  //   // mp3tag.tags.v2.TIT2 = "爱一个人好难111";
+  //   // mp3tag.tags.v2.TALB = "她的时光111";
+  //   // mp3tag.save();
+  // } catch (e) {
+  //   console.error(e);
+  // }
 }
 
 export {
